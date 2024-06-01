4f8e8be3461a8a06eac99ee450e4ef3583789369
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-06-01 22:31:44 +0300

    Шаг 11

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -30,6 +30,13 @@ async function publishPost(id: string): Promise<void> {
   await Router.push('/');
 }
 
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE',
+  });
+  Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
   const { data: session, status } = useSession();
   if (status === 'loading') {
