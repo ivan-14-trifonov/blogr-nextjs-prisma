077401d1f73125732594a74fbd15c5967e5b5b17
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-06-01 22:27:05 +0300

    Шаг 8 Исправления

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,13 +1,11 @@
-import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
-  const { title, content } = req.body;
+  const { title, content, session } = req.body;
 
-  const session = await getSession({ req });
   const result = await prisma.post.create({
     data: {
       title: title,
